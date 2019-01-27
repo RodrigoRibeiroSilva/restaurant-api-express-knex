6270a81a755c6b9710119fe00b8aef2078e807a3
RodrigoRibeiroSilva <rodrigo94.ribeiro.silva@gmail.com>
2019-01-27 10:44:37 -0300

    Implementada a classe do servidor

---
+++ src/app/server/server.ts
@@ -0,0 +1,31 @@
+import * as express from "express";
+import * as bodyParser from "body-parser";
+
+import { environment } from '../common/environment'
+
+
+export class Server {
+    
+    public app: express.Application
+    private port : any
+
+    constructor() {
+        this.app = express();
+        this.port = environment.server.port
+
+        this.config();
+    }
+
+    initServer(){
+        return this.app.listen(this.port, () =>{
+            console.log('Server is listening on PORT:', this.port)
+        })
+    }
+
+    private config(): void {
+        // support application/json
+        this.app.use(bodyParser.json());
+        //support application/x-www-form-urlencoded post data
+        this.app.use(bodyParser.urlencoded({ extended: false }));
+    }
+}
